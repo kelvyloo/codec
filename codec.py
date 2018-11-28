26c1d8f3ebe5064abb27835f39dc341aa90423cb
Kelvin Lu <38535362+kelvyloo@users.noreply.github.com>
2018-11-27 23:19:30 -0500

    Add files via upload
    Cleaned up code: better variable names and function definitions for compression techniques. Also added compression/zip to increase compression ratio

--- codec.py
+++ codec.py
@@ -2,9 +2,9 @@
 # coding: utf-8
 
 # # ECES 434 Intro to Applied DSP
-# ## Final Project
+# ## Final Project: Audio Codec
 
-# In[153]:
+# In[138]:
 
 
 import librosa
@@ -13,92 +13,93 @@ import sys
 import pickle
 import time
 import IPython.display as ipd
+import io
 
 
 # ---
 # ## Import Audio File
 # *Load in an audio file to test with codec.*
 
-# In[154]:
+# In[139]:
 
 
 filename = "Voices/female3.wav"
+#filename = "taxman.wav"
 x, sr = librosa.load(filename)
 
 
-# ---
-# ## Encode/Decode
-# 
-# *Define your encode and decode functions.*
+# ## Encoder/Decoder helper functions
 
-# In[155]:
+# In[140]:
 
 
-# encode function here
-def encode(x, sr=22050, hop=512, n=30):
+def mask(x, sr, num_freq):
     
-    D = librosa.stft(x).T
+    X = librosa.stft(x).T
     
-    a = np.zeros((D.shape[0], 3, n))
+    masked_sig = np.zeros((X.shape[0], 3, num_freq))
     
-    q = 3
+    triangle_width = 3
     
     index = 0
     
-    for dft in D:
-    
-        A = []
-        F = []
-        P = []
+    for dft in X:
+        
+        amp_vec = []
+        freq_vec = []
+        phase_vec = []
 
         length = np.shape(dft)[0]
-        magSpec = np.abs(dft)
+        mag = np.abs(dft)
         phase = np.angle(dft)
         freq = (np.arange(length)/length) * (sr//2)
 
-        for peak in range(n):
-            i = np.argmax(magSpec)
-            h = magSpec[i]
-            A.append(h)
-            F.append(freq[i])
-            P.append(phase[i])
+        for peak in range(num_freq):
+            i = np.argmax(mag)
+            h = mag[i]
             
-            halfWin = int(np.floor(i/(q*2)))
-            rampUp = np.linspace(0, h, halfWin, endpoint=False)
-            rampDown = np.flip(rampUp, axis=0)
+            amp_vec.append(h)
+            freq_vec.append(freq[i])
+            phase_vec.append(phase[i])
             
-            triangle = np.concatenate([rampUp, np.array(h), rampDown], axis=None)
+            half_win = int(np.floor(i/(triangle_width*2)))
+            ramp_up = np.linspace(0, h, half_win, endpoint=False)
+            ramp_down = np.flip(ramp_up, axis=0)
+            
+            triangle = np.concatenate([ramp_up, np.array(h), ramp_down], axis=None)
+            
             zeros = np.zeros(length)
             padded = np.concatenate((zeros,triangle,zeros),axis = 0)
-            shift = length + halfWin - i
+            
+            shift = length + half_win - i
             tri = padded[shift:(shift + length)]
             
-            phase = np.array([0 if magSpec[idx] <= tri[idx] else phase[idx] for idx in range(length)])
-            magSpec = np.array([0 if (magSpec[idx] <= tri[idx]) else magSpec[idx] for idx in range(length)])
+            mag = np.array([0 if (mag[j] <= tri[j]) else mag[j] for j in range(length)])
 
-        a[index][0] = A
-        a[index][1] = F
-        a[index][2] = P
+        masked_sig[index][0] = amp_vec
+        masked_sig[index][1] = freq_vec
+        masked_sig[index][2] = phase_vec
 
         index += 1
     
-    return a
+    return masked_sig
 
 
-# In[156]:
+# In[141]:
 
 
-# decode function here
-def decode(x, sr=22050, hop_size=512, num_freq=30):
-    y_out = np.zeros((1, 0))
-    dur = hop_size/sr
+def unmask(x, sr, num_freq):
+    
+    unmasked_sig = np.zeros((1, 0))
+    dur = 512/sr
     
     for frame in x:
+        
         amp = frame[0]
         freq = frame[1]
         phase = frame[2]
         
-        t = np.linspace(0, dur, hop_size)
+        t = np.linspace(0, dur, dur * sr)
         
         freq.shape = (num_freq, 1)
         amp.shape = (1, num_freq)
@@ -108,16 +109,69 @@ def decode(x, sr=22050, hop_size=512, num_freq=30):
         cos_matrix = np.cos(2 * np.pi * freq * t + phase)
         cos_sum = np.dot(amp, cos_matrix)
         
-        y_out = np.concatenate((y_out, cos_sum), axis=1)
+        unmasked_sig = np.concatenate((unmasked_sig, cos_sum), axis=1)
         
-    return y_out
+    return unmasked_sig
+
+
+# In[142]:
+
+
+def compress(x):
+    
+    compressed_sig = io.BytesIO()
+    np.savez_compressed(compressed_sig, x)
+    
+    return compressed_sig
+
+
+# In[143]:
+
+
+def decompress(x):
+    
+    x.seek(0)
+    decompressed = np.load(x)['arr_0']
+    
+    return decompressed
+
+
+# ---
+# ## Encode/Decode
+# 
+# *Definitions for encode and decode functions.*
+
+# In[144]:
+
+
+# encode function
+def encode(x, sr=22050, num_freq=20):
+    
+    x_mask = mask(x, sr, num_freq)
+    
+    x_encoded = compress(x_mask)
+    
+    return x_encoded
+
+
+# In[145]:
+
+
+# decode function
+def decode(x, sr=22050, num_freq=20):
+    
+    decompressed = decompress(x)
+    
+    x_decoded = unmask(decompressed, sr, num_freq)
+    
+    return x_decoded
 
 
 # ---
 # ## Runtime
 # *Encode and decode the audio. Time the processes.*
 
-# In[157]:
+# In[146]:
 
 
 # Encode the audio file and also calculate elapsed time
@@ -137,7 +191,7 @@ total_time = decode_elapse_t + encode_elapse_t
 # ## Compression Ratio
 # *Compare the sizes of the original and encoded structures.*
 
-# In[158]:
+# In[147]:
 
 
 def compression_ratio(original, encoded):
@@ -155,7 +209,7 @@ compress_ratio = compression_ratio(x, x_encoded)
 # ## SNR
 # *Compare the original signal content to the decoded version*
 
-# In[159]:
+# In[148]:
 
 
 def signal_to_noise(original, decoded):
@@ -185,7 +239,7 @@ snr = signal_to_noise(x, x_decoded)
 # ## Evaluate Codec
 # *Print out evalutation of codec. Listen to the results*
 
-# In[160]:
+# In[149]:
 
 
 print("Total elapsed time for codec:", total_time)
@@ -195,14 +249,14 @@ print("Compression Ratio:", compress_ratio, "\n")
 print("Signal-to-Noise Ratio (dB):", snr)
 
 
-# In[161]:
+# In[150]:
 
 
 # Original
 ipd.Audio(x, rate = sr)
 
 
-# In[162]:
+# In[151]:
 
 
 # Encoded/Decoded
